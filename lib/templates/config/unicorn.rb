b582f4e1a566d8cd3488163b3064fc706cebd7e6
Zach Pendleton <zachpendleton@gmail.com>
2011-08-14 22:44:59 -0600

    [new] Added unicorn options if app isn't on Heroku.

--- lib/templates/config/unicorn.rb
+++ lib/templates/config/unicorn.rb
@@ -1,10 +1,12 @@
 worker_processes 3
 timeout 30
 
-# docroot = File.join(File.expand_path(File.dirname(__FILE__)), "..")
+<% if @noheroku -%>
+docroot = File.join(File.expand_path(File.dirname(__FILE__)), "..")
 
-# listen "#{docroot}/tmp/sockets/unicorn.sock"
-# pid "#{docroot}/tmp/pids/unicorn.pid"
+listen "#{docroot}/tmp/sockets/unicorn.sock"
+pid "#{docroot}/tmp/pids/unicorn.pid"
 
-# stderr_path "#{docroot}/log/stderr.log"
-# stdout_path "#{docroot}/log/stdout.log"
+stderr_path "#{docroot}/log/stderr.log"
+stdout_path "#{docroot}/log/stdout.log"
+<% end -%>
